591b5cbbf774549146e23e5e7973d492553458d3
RoyOkite <royokite@gmail.com>
2022-08-24 15:56:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// DOMContentLoaded!
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    console.log('Haha we up! The DOM has loaded!');
+    document.getElementById('text').innerHTML = 'This is really cool!';
+})
+
+console.log('Chill, DOM yet to load!');
